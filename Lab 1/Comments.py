0d2302f017f2c5076be2fe93979e660f38a2bf8f
Dilshat <rakhmatulindilshat@gmail.com>
2024-01-17 12:23:26 +0600

    third task

---
+++ Lab 1/Comments.py
@@ -0,0 +1,22 @@
+#Creating a Comment
+
+#This is a comment
+print("Hello, World!")
+
+print("Hello, World!") #This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#Multiline Comment
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
