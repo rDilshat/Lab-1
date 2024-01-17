33bac27e04292bb6cc9073a85d1bfa0768c3f8c1
Dilshat <rakhmatulindilshat@gmail.com>
2024-01-17 12:13:19 +0600

    second task

---
+++ Lab 1/Syntax.py
@@ -0,0 +1,18 @@
+#Python Indentation
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+#Python Variables
+    
+x = 5
+y = "Hello, World!"
+
+#Python Comments
+
+#This is comment
+
+print("Hello, world!")
